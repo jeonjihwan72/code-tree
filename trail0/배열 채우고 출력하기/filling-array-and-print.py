ab30e24c00ada8cf575638a83dd461609b4c0921
Jeon Jihwan <127182495+jeonjihwan72@users.noreply.github.com>
2026-07-15 11:45:25 +0900

    [Passed] 배열 채우고 출력하기 (57ms, 16MB)

--- trail0/배열 채우고 출력하기/filling-array-and-print.py
+++ trail0/배열 채우고 출력하기/filling-array-and-print.py
@@ -1,4 +1,7 @@
 input_list = input().split()
 
-for i in range(-1, -(len(input_list)+1), -1):
-    print(input_list[i], end='')
+# for i in range(-1, -(len(input_list)+1), -1):
+#     print(input_list[i], end='')
+
+for i in range(len(input_list)):
+    print(input_list[::-1][i], end='')
