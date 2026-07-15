478c714c061106e76251bb88dff7ca0e2b015324
Jeon Jihwan <127182495+jeonjihwan72@users.noreply.github.com>
2026-07-16 01:22:24 +0900

    [Passed] 짝수만 거꾸로 출력하기 (49ms, 16MB)

---
+++ trail0/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.py
@@ -0,0 +1,14 @@
+N = int(input())
+num_list = map(int, input().split())
+
+result = []
+
+for num in num_list:
+    if num % 2 == 0:
+        result.append(num)
+        
+result.reverse()
+# result = result[::-1]
+
+for answer in result:
+    print(answer, end=" ")
